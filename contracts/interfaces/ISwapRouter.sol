5bfb06a8709aff50c96e54de59d6015b87029dc0
notedd <188998913@qq.com>
2023-06-25 07:40:30 +0000

    test

--- contracts/interfaces/ISwapRouter.sol
+++ contracts/interfaces/ISwapRouter.sol
@@ -8,19 +8,22 @@ import '@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3SwapCallback.so
 /// @notice Functions for swapping tokens via Uniswap V3
 interface ISwapRouter is IUniswapV3SwapCallback {
     struct ExactInputSingleParams {
-        address tokenIn;
-        address tokenOut;
-        uint24 fee;
-        address recipient;
-        uint256 deadline;
-        uint256 amountIn;
-        uint256 amountOutMinimum;
-        uint160 sqrtPriceLimitX96;
+        address tokenIn; //输入代币地址
+        address tokenOut; //输出代币地址
+        uint24 fee;//手续费率
+        address recipient;//接收交易输出的地址
+        uint256 deadline;//交易截止时间
+        uint256 amountIn;//输入代币数量
+        uint256 amountOutMinimum;//最小输出代币数量
+        uint160 sqrtPriceLimitX96;//自定义价格范围上限
     }
 
     /// @notice Swaps `amountIn` of one token for as much as possible of another token
     /// @param params The parameters necessary for the swap, encoded as `ExactInputSingleParams` in calldata
     /// @return amountOut The amount of the received token
+    // ExactInputSingle 方法用于实现在一次交易中将某种代币按照指定的兑换比例、手续费率和价格范围进行兑换的操作。ExactInputSingleParams 
+    // 结构体定义了该方法需要的各个参数，包括输入代币和输出代币地址、手续费率、接收交易输出的地址、交易截止时间、输入代币数量、最小输出代币数量、
+    // 自定义价格范围上限等。在使用 ExactInputSingle 方法时，需要构造该结构体，并将其作为方法的输入参数进行传递。
     function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
 
     struct ExactInputParams {
